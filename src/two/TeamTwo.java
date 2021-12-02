2f04ef6cee2bbd2166e58786aa15f7d461cfdb0c
nazar <nazar@DESKTOP-MOV89AK>
2021-12-01 22:02:26 -0500

    getInfo method added.

--- src/two/TeamTwo.java
+++ src/two/TeamTwo.java
@@ -3,6 +3,12 @@ package two;
 public class TeamTwo {
 
 	public static void main(String[] args) {
+		
+		getInfo("Atiq");
+		getInfo("Ali");
+		getInfo("Misha");
+		getInfo("Jamie");
+		
 		System.out.println("Team Two Initial code...");
 		
 		System.out.println("Let's make some conflicts...");
@@ -43,5 +49,9 @@ public class TeamTwo {
 		System.out.println("first change and merge");
 		System.out.println("Merge to Master Branch");
 
+	}
+	
+	public static void getInfo(String name) {
+		System.out.println(name);
 	}
 }
